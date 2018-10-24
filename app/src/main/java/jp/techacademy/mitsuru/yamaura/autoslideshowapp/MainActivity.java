588df6b99d4c90122d11241be317e379af6a5720
Mitsuru\mitsuru2 <komayou326@gmail.com>
2018-10-24 18:16:55 +0900

    課題

--- app/src/main/java/jp/techacademy/mitsuru/yamaura/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/mitsuru/yamaura/autoslideshowapp/MainActivity.java
@@ -17,17 +17,19 @@ import android.widget.Toast;
 import android.view.View;
 import android.widget.Button;
 
-import java.Timer;
-import java.TimerTask;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int PERMISSIONS_REQUEST_CODE = 100;
 
-    //Timer mtimer;
+    //Timer mTimer;
     double mTimerSec = 0.0f;
 
     //Handler mHandler = new Handler;
+
+    Cursor cursor1 = null;
 
     Button mNextButton;
     Button mStartButton;
@@ -39,12 +41,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(this);
+
         mStartButton = (Button) findViewById(R.id.start_button);
+        mStartButton.setOnClickListener(this);
+
         mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(this);
 
 
         //  Andorid6.0以降の場合
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             //  パーミッションの許可状態を確認する
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 //  許可されている。呼び出し
@@ -52,15 +59,49 @@ public class MainActivity extends AppCompatActivity {
 
             } else {
                 //  許可されていないので許可ダイアログを表示する
-                Toast.makeText(this,"パーミッションを許可してください", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "パーミッションを許可してください", Toast.LENGTH_SHORT).show();
                 requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
             }
-        //  Andorid5.0以下
-        }else{
+            //  Andorid5.0以下
+        } else {
             getContentsInfo();
+        }
+
+        @Override
+        public void onClick (View v){
+            //  タップをした時の処理。ボタンで分岐
+            if (cursor1 == null) {
+                //  画像の情報を取得する
+                ContentResolver resolver = getContentResolver();
+                Cursor cursor = resolver.query(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,//  データの種類
+                        null,    //  項目（null=全項目）
+                        null,     //  フィルタ条件（null=フィルタなし）
+                        null,  //  フィルタ用パラメータ
+                        null     //  ソート（null=ソートなし）
+                );
+            }
+            if (v.getID() == R.id.mStartButton) {
+
+                //  Timerを使用して画像を時間で再生する。
+                //  また、その間は戻ると進むをタップできないようにする
+                getContentsInfo();
+
+            } else if (v.getID() == R.id.mNextButton) {
+                //if (cursor1.moveToNext()) {
+                    //  画像を取得する
+                //}
+
+            } else if (v.getID() == R.id.mPrevButton) {
+                //if (cursor1.moveToPrevious()) {
+                    //  前の画像を取得する
+                //}
+            }
         }
     }
 
+
+    //  パーミッションの選択結果の受け取りメソッド
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResult){
         switch (requestCode){
@@ -74,28 +115,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void getContentsInfo(){
+    //  画像の取得処理のメソッド
+    private void getContentsInfo() {
 
-        //  画像の情報を取得する
         ContentResolver resolver = getContentResolver();
         Cursor cursor = resolver.query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI,//  データの種類
-                null,
-                null,
-                null,
-                null
+                null,    //  項目（null=全項目）
+                null,     //  フィルタ条件（null=フィルタなし）
+                null,  //  フィルタ用パラメータ
+                null     //  ソート（null=ソートなし）
         );
 
-        if(cursor.moveToFirst()) {
-            //  indexからIDを取得し、そのIDから画像のURIを取得する
-            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            Long id = cursor.getLong(fieldIndex);
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
-            ImageView imageView = (ImageView) findViewById(R.id.imageView);
-            imageView.setImageURI(imageUri);
-        } while(cursor.moveToNext());
+        if (cursor.moveToFirst()) {
+                //  indexからIDを取得し、そのIDから画像のURIを取得する
+                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                Long id = cursor.getLong(fieldIndex);
+                Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageURI(imageUri);
+        }
+        cursor.close();
     }
-    cursor.close();
 }
