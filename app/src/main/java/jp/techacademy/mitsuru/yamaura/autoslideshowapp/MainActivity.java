263d0dcc8af4193951beeee48bf9f5a90d2f4c0c
Mitsuru\mitsuru2 <komayou326@gmail.com>
2018-11-06 23:28:32 +0900

    完成

--- app/src/main/java/jp/techacademy/mitsuru/yamaura/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/mitsuru/yamaura/autoslideshowapp/MainActivity.java
@@ -135,6 +135,7 @@ public class MainActivity extends AppCompatActivity {
                     mNextButton.setEnabled(true);
                     mPrevButton.setEnabled(true);
                 }
+                
             }
         });
 
